ee86499c0bfe0b238ce72f25bdb008d6ac7b69c9
Kostas Demiris <4679365+wehappyfew@users.noreply.github.com>
2018-08-06 23:51:45 +0300

    added the locust specific params

--- models/commands.model.js
+++ models/commands.model.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 // DB Entities Map
 
 const CommandSchema = mongoose.Schema({
-    quantity: {type: Number, required: true, unique: false, index: false},
-    userId: {type: Number, required: true, unique: false, index: false},
+    // quantity: {type: Number, required: true, unique: false, index: false},
+    // userId: {type: Number, required: true, unique: false, index: false},
+    target_url: {type: String, required: true, unique: false, index: false},
+    virtual_users: {type: Number, required: true, unique: false, index: false},
+    test_duration: {type: Number, required: true, unique: false, index: false},
 }, {collection : 'Command'});
 
 let CommandsModel = mongoose.model('Command', CommandSchema);
